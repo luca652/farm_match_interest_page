e77d3374eddca5d1ef3dd839431711a05d1f8926
luca gastaldi <lucagastaldi82@gmail.com>
2024-05-02 15:36:59 +0100

    Add responsive navbar and menu button functionality
    - Implement Stimulus controller for navbar functionality
    - Implement logic to toggle navbar options based on screen size
    - Add menu button functionality to toggle visibility of navbar options
    - Update CSS styles to adjust navbar layout for different screen sizes

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+
+  static targets = ["navbarLinks", "burgerButton"]
+
+  connect() {
+    console.log("Ecchime!");
+    this.navbarToggle();
+  }
+
+  navbarToggle() {
+    const navbarLinks = this.navbarLinksTarget;
+
+    if (window.innerWidth < 930) {
+      navbarLinks.classList.add("navbar__options--mobile");
+      navbarLinks.classList.add("hidden");
+    } else {
+      navbarLinks.classList.remove("navbar__options--mobile");
+      navbarLinks.classList.remove("hidden");
+    }
+  }
+
+  menuToggle() {
+    const navbarLinks = this.navbarLinksTarget;
+    if (navbarLinks.classList.contains("hidden")) {
+      navbarLinks.classList.remove("hidden");
+    } else {
+      navbarLinks.classList.add("hidden");
+    }
+  }
+}
