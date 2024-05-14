22ae81794c8c4ccc85e1f5b60fa8522c9bc127e0
luca gastaldi <lucagastaldi82@gmail.com>
2024-05-14 12:58:50 +0100

    Add devise and create potential_users page Add devise gem Create potential_users route and page, which requires log in Prevent users from registering by removing :registerable from user model Style log in form

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'simple_form'
 
 gem 'email_address'
 
+gem 'devise'
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#welcome"
   get "register-your-interest", to: "potential_users#register"
-  resources :potential_users, only: [:new, :create]
+  resources :potential_users, only: [:new, :create, :index]
 end
