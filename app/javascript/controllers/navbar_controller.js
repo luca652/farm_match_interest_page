95de3efef2997cb62f1ae6662bda8eb7a0901fe9
luca gastaldi <lucagastaldi82@gmail.com>
2024-06-13 12:07:06 +0100

    Add media queries to make page responsive

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -12,21 +12,12 @@ export default class extends Controller {
   navbarToggle() {
     const navbarLinks = this.navbarLinksTarget;
 
-    if (window.innerWidth < 930) {
+    if (window.innerWidth < 768) {
       navbarLinks.classList.add("navbar__options--mobile");
       navbarLinks.classList.add("hidden");
     } else {
       navbarLinks.classList.remove("navbar__options--mobile");
       navbarLinks.classList.remove("hidden");
-    }
-  }
-
-  menuToggle() {
-    const navbarLinks = this.navbarLinksTarget;
-    if (navbarLinks.classList.contains("hidden")) {
-      navbarLinks.classList.remove("hidden");
-    } else {
-      navbarLinks.classList.add("hidden");
     }
   }
 }
